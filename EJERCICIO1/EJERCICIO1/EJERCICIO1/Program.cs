6da7c8136323ef7d91c4e4dfe15499084f874102
karladenisse3 <56401744+karladenisse3@users.noreply.github.com>
2019-10-31 05:14:14 -0500

    Add files via upload

---
+++ EJERCICIO1/EJERCICIO1/EJERCICIO1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EJERCICIO1.ClaseEncapsulamiento;
+
+namespace EJERCICIO1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Int16 x = Con Console.ReadLine();
+
+
+            Prestamo prestamo=
+                  new Prestamo(); //instacia
+            prestamo.Horas = 5;
+            Console.WriteLine(Prestamo.);
+            Console.Read();
+        }
+    }
+}
